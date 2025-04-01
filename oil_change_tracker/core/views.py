a294f3e5ce2a236e6526515083b74ddd4e39538f
JNaiman97 <jakubnaiman97@gmail.com>
2025-04-01 17:13:30 +0200

    apps_urls

--- oil_change_tracker/core/views.py
+++ oil_change_tracker/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    #a = 10 / 0
+    return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
