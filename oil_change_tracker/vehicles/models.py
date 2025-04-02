34743c29a0f0ae508d5f05f38a07b8cc285915db
JNaiman97 <jakubnaiman97@gmail.com>
2025-04-02 23:26:14 +0200

    vehicles_model

--- oil_change_tracker/vehicles/models.py
+++ oil_change_tracker/vehicles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+# Create your models here.
+
+class Vehicle(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='vehicle_photos/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
