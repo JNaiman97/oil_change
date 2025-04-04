b95d224c037e5590593bef0817be6bef8fd7161a
JNaiman97 <jakubnaiman97@gmail.com>
2025-04-04 04:16:28 +0200

    Vehicle_views

--- oil_change_tracker/vehicles/views.py
+++ oil_change_tracker/vehicles/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import vehicle
+from .forms import VehicleForm
 
-# Create your views here.
+class vehicleListView(LoginRequiredMixin, ListView):
+    model = vehicle
+    template_name = 'vehicle_list.html'
+    context_object_name = 'vehicles'
+
+class vehicleDetailView(LoginRequiredMixin, DetailView):
+    model = vehicle
+    template_name = 'vehicle_detail.html'
+    context_object_name = 'vehicle'
+
+class vehicleCreateView(LoginRequiredMixin, CreateView):
+    model = vehicle
+    form_class = VehicleForm
+    template_name = 'vehicle_form.html'
+    success_url = reverse_lazy('vehicle-list')
+
+class vehicleUpdateView(LoginRequiredMixin, UpdateView):
+    model = vehicle
+    form_class = VehicleForm
+    template_name = 'vehicle_form.html'
+    success_url = reverse_lazy('vehicle-list')
+
+class vehicleDeleteView(LoginRequiredMixin, DeleteView):
+    model = vehicle
+    template_name = 'vehicle_confirm_delete.html'
+    success_url = reverse_lazy('vehicle-list')
