eebd4ea7555dc19b25092982a7ce2d65d218d471
JNaiman97 <jakubnaiman97@gmail.com>
2025-04-04 12:06:54 +0200

    superadmin

--- oil_change_tracker/vehicles/admin.py
+++ oil_change_tracker/vehicles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Vehicle, OilChangeRecord
 
-# Register your models here.
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'engine', 'photo')
+    search_fields = ('name', 'owner__username')
+
+@admin.register(OilChangeRecord)
+class OilChangeRecordAdmin(admin.ModelAdmin):
+    list_display = ('vehicle', 'date', 'kilometrage', 'next_reminder')
+    search_fields = ('vehicle__name', 'vehicle__owner__username')
+
