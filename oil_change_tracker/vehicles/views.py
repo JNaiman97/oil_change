f7fb0990c7a7dd9477c0404c7388d985425d13ee
JNaiman97 <jakubnaiman97@gmail.com>
2025-04-04 11:14:12 +0200

    vehicle_views_redo

--- oil_change_tracker/vehicles/views.py
+++ oil_change_tracker/vehicles/views.py
@@ -3,16 +3,24 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy
 from .models import Vehicle
 from .forms import VehicleForm
+from .models import OilChangeRecord
+from .forms import OilChangeForm
 
 class vehicleListView(LoginRequiredMixin, ListView):
     model = Vehicle
     template_name = 'vehicle_list.html'
     context_object_name = 'vehicles'
 
+    def get_queryset(self):
+        return Vehicle.objects.filter(owner=self.request.user)
+
 class vehicleDetailView(LoginRequiredMixin, DetailView):
     model = Vehicle
     template_name = 'vehicle_detail.html'
-    context_object_name = 'vehicles'
+    context_object_name = 'vehicle'
+
+    def get_queryset(self):
+        return Vehicle.objects.filter(owner=self.request.user)
 
 class vehicleCreateView(LoginRequiredMixin, CreateView):
     model = Vehicle
@@ -20,13 +28,23 @@ class vehicleCreateView(LoginRequiredMixin, CreateView):
     template_name = 'vehicle_form.html'
     success_url = reverse_lazy('vehicle_list')
 
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
 class vehicleUpdateView(LoginRequiredMixin, UpdateView):
     model = Vehicle
     form_class = VehicleForm
     template_name = 'vehicle_form.html'
     success_url = reverse_lazy('vehicle_list')
 
+    def get_queryset(self):
+        return Vehicle.objects.filter(owner=self.request.user)
+
 class vehicleDeleteView(LoginRequiredMixin, DeleteView):
     model = Vehicle
     template_name = 'vehicle_confirm_delete.html'
     success_url = reverse_lazy('vehicle_list')
+
+    def get_queryset(self):
+        return Vehicle.objects.filter(owner=self.request.user)
