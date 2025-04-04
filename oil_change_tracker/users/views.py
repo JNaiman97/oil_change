fd4844d301e8d4aa1c1927ee2c322dfd07e9d53b
JNaiman97 <jakubnaiman97@gmail.com>
2025-04-04 03:23:43 +0200

    _login_logout_register_WIP

--- oil_change_tracker/users/views.py
+++ oil_change_tracker/users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth import login
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f"Account created for {user.username}!")
+            return redirect('')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
