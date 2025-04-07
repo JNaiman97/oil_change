f9ac169c23d9bbe64a0c088424e424f2a42192e1
JNaiman97 <jakubnaiman97@gmail.com>
2025-04-08 00:54:44 +0200

    vehicle model_add_brand_and_migrate

--- oil_change_tracker/vehicles/models/vehicle.py
+++ oil_change_tracker/vehicles/models/vehicle.py
@@ -4,6 +4,7 @@ from django.conf import settings
 class Vehicle(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100, blank=True, null=True)
     model = models.CharField(max_length=100, blank=True, null=True)
     engine = models.CharField(max_length=100, blank=True, null=True)
     photo = models.ImageField(upload_to='vehicle_photos/', blank=True, null=True)
