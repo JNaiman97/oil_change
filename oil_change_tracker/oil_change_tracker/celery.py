46d1e9f33bf73ad155897cbc59248253f82168f1
JNaiman97 <jakubnaiman97@gmail.com>
2025-04-08 01:56:05 +0200

    no_more_celery

--- oil_change_tracker/oil_change_tracker/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oil_change_tracker.settings')
-
-app = Celery('oil_change_tracker')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
--- oil_change_tracker/oil_change_tracker/settings.py
+++ oil_change_tracker/oil_change_tracker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,8 +134,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'vehicle_list'
 LOGOUT_REDIRECT_URL = 'home'
 
-# Nastavení Redis Celery
+# nastavení odesílání pomocí gmailu
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'jakubnaiman97@gmail.com'
+EMAIL_HOST_PASSWORD = 'fxxb emcl fzot ukhw'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
