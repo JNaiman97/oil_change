6deaf3a699b1ea38ca9c00fbdcfb34b89b7a110c
JNaiman97 <jakubnaiman97@gmail.com>
2025-04-08 01:07:32 +0200

    celery_setup_tasks.py

--- oil_change_tracker/oil_change_tracker/settings.py
+++ oil_change_tracker/oil_change_tracker/settings.py
@@ -132,3 +132,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'vehicle_list'
 LOGOUT_REDIRECT_URL = 'home'
+
+# Nastavení Redis Celery
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
