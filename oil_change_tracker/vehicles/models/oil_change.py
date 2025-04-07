1be4b2f57c3aa057510d4b7ab6ff48c5a903c49e
JNaiman97 <jakubnaiman97@gmail.com>
2025-04-07 21:25:11 +0200

    small changes to models + migrations

--- oil_change_tracker/vehicles/models/oil_change.py
+++ oil_change_tracker/vehicles/models/oil_change.py
@@ -3,9 +3,9 @@ from .vehicle import Vehicle
 
 class OilChangeRecord(models.Model):
     vehicle = models.OneToOneField(Vehicle, on_delete=models.CASCADE, related_name="oil_changes")
-    date = models.DateField()
-    kilometrage = models.PositiveIntegerField()
-    next_reminder = models.DateField()
+    date = models.DateField(blank=True, null=True)
+    kilometrage = models.PositiveIntegerField(blank=True, null=True)
+    next_reminder = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.vehicle.name} - {self.date}"
--- oil_change_tracker/vehicles/models/vehicle.py
+++ oil_change_tracker/vehicles/models/vehicle.py
@@ -4,7 +4,8 @@ from django.conf import settings
 class Vehicle(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    engine = models.CharField(max_length=100)
+    model = models.CharField(max_length=100, blank=True, null=True)
+    engine = models.CharField(max_length=100, blank=True, null=True)
     photo = models.ImageField(upload_to='vehicle_photos/', blank=True, null=True)
 
     def __str__(self):
