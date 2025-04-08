5d6be27f4f6be96f5ffcd5a11507ad001068e8f8
JNaiman97 <jakubnaiman97@gmail.com>
2025-04-08 13:25:25 +0200

    collect_static + settings_update

--- oil_change_tracker/oil_change_tracker/settings.py
+++ oil_change_tracker/oil_change_tracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6vwy_g*6(0o!^i-tc#x$6gkt+5#0wwx$)6=7omepb9za+90jjh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [jnaiman97.pythonanywhere.com]
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
